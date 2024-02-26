393358b30484be8840927653134566da2733f433
Simon <simon.bosseler@student.vinci.be>
2024-02-26 11:41:22 +0100

    setup : mise en page de la JavaDoc dans la classe Graph

--- src/Graph.java
+++ src/Graph.java
@@ -7,10 +7,22 @@ public class Graph {
   public Graph(File cities, File roads) {
   }
 
+  /**
+   * Méthode qui calcule l'itinéraire entre deux villes avec le moins de routes possible
+   * Utiliser la méthode de BFS
+   * @param depart le départ de l'itinéraire
+   * @param destination la destination de l'itinéraire
+   */
   public void calculerItineraireMinimisantNombreRoutes(String depart, String destination) {
     
   }
 
+  /**
+   * Méthode qui calcule l'itinéraire entre deux villes avec le moins de kilomètres
+   * Utiliser la méthode de Dijkstra
+   * @param depart le départ de l'itinéraire
+   * @param destination la destination de l'itinéraire
+   */
   public void calculerItineraireMinimisantKm(String depart, String destination) {
   }
 }
