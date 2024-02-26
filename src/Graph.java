e02e1ab621e5c931a7144178e47047f9725261fe
Nicolas <nicolas.venturini@student.vinci.be>
2024-02-26 11:28:27 +0100

    change : param

--- src/Graph.java
+++ src/Graph.java
@@ -7,10 +7,10 @@ public class Graph {
   public Graph(File cities, File roads) {
   }
 
-  public void calculerItineraireMinimisantNombreRoutes(String berlin, String madrid) {
+  public void calculerItineraireMinimisantNombreRoutes(String depart, String destination) {
     
   }
 
-  public void calculerItineraireMinimisantKm(String berlin, String madrid) {
+  public void calculerItineraireMinimisantKm(String depart, String destination) {
   }
 }
