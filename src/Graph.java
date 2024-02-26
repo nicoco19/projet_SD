ee34e307ef32c2e026dfcae32f101f8e486f2838
Nicolas <nicolas.venturini@student.vinci.be>
2024-02-26 11:25:02 +0100

    creat : file graph

---
+++ src/Graph.java
@@ -0,0 +1,16 @@
+package src;
+
+import java.io.File;
+
+public class Graph {
+
+  public Graph(File cities, File roads) {
+  }
+
+  public void calculerItineraireMinimisantNombreRoutes(String berlin, String madrid) {
+    
+  }
+
+  public void calculerItineraireMinimisantKm(String berlin, String madrid) {
+  }
+}
